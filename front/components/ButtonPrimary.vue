ba67f7c9be79bdc590f0654534c17909148c20de
mkorczak <marco.korczak@orange.fr>
2024-05-03 17:13:54 +0200

    Added 3 buttons components

---
+++ front/components/ButtonPrimary.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-primary rounded-full">{{title}}</button>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String
+})
+</script>
+
+<style scoped>
+
+</style>
