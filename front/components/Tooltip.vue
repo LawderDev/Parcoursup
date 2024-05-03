fb020cf8202eaea5c445393cb43e086e6cd553ca
noan-miage <n.lecarboulec@gmail.com>
2024-05-03 18:01:17 +0200

    tooltip

---
+++ front/components/Tooltip.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tooltip tooltip-right" :data-tip="content">
+    <button class="btn rounded-full w-16 h-12 text-2xl"><slot></slot></button>
+    
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  content: String
+})
+</script>
+
+<style scoped></style>
