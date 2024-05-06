55e1e47f69338bd09dbb9c2525339396befefdbd
Vo1ucia <157504371+Vo1ucia@users.noreply.github.com>
2024-05-06 14:05:14 +0200

    Composant date fait avec une nouvelle lib

---
+++ front/components/Date.vue
@@ -0,0 +1,20 @@
+<template>
+  <VueDatePicker v-model="date"></VueDatePicker>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+const today = new Date();
+const date = ref(null);
+
+watch(date, (newVal) => {
+  if (newVal && newVal < today) {
+    // Reset to today's date if selected date is before today
+    date.value = today;
+  }
+}, { immediate: true }); // Validate on initial render
+
+</script>
--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,4 +2,5 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', "@nuxt/image",'@pinia/nuxt'],
+  transpile: ['@vuepic/vue-datepicker']
 })
