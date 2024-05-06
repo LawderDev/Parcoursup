39ee0d69ef222bb7b1468afc3b4ca8093100eb89
matteo <matteo.davoigneau@gmail.com>
2024-05-06 11:20:16 +0200

    adding gale_shapley

--- back/api_restfull.py
+++ back/api_restfull.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from collections import Counter
 import sqlite3
 import os
 
@@ -39,6 +40,48 @@ def get_hello_world():
     else:
         return jsonify({'error': "nul"}), 50
 
+@app.route('/api/gale_shapley', methods=['POST'])
+def gale_shapley_route():
+    data = request.json["data"]
+    if 'men_preferences' in data and 'women_preferences' in data:
+        men_preferences = data['men_preferences']
+        women_preferences = data['women_preferences']
+        res = gale_shapley(women_preferences, men_preferences)
+        return jsonify(result=res)
+    else:
+        return jsonify({'error': 'Invalid request'}), 400
+
+def gale_shapley(women_preferences, men_preferences):
+    waiting_list = []
+    proposals = {}
+    count = 0
+    women_available = {man: list(women_preferences.keys()) for man in men_preferences.keys()}
+    men_available = men_preferences.copy()
+
+    while len(waiting_list) < len(men_preferences):
+        for man in men_preferences.keys():
+            if man not in waiting_list:
+                women = men_available[man]
+                best_choice = women[0]
+
+                proposals[(man, best_choice)] = (women_preferences[best_choice].index(man), men_preferences[man].index(best_choice))
+                del women_available[man][0]
+
+        overlays = Counter([key[1] for key in proposals.keys()])
+        for woman in overlays.keys():
+            if overlays[woman] > 1:
+                pairs_to_drop = sorted({pair: proposals[pair] for pair in proposals.keys()
+                                        if woman in pair}.items(),
+                                    key=lambda x: (x[1][1], x[1][0]))[1:]
+                for p_to_drop in pairs_to_drop:
+                    del proposals[p_to_drop[0]]
+                    del men_available[p_to_drop[0][0]][0]
+
+        waiting_list = [man[0] for man in proposals.keys()]
+        count += 1
+
+    matched_pairs = [{'man': pair[0], 'woman': pair[1]} for pair in proposals.keys()]
+    return {'matched_pairs': matched_pairs}
 
 if __name__ == '__main__':
     app.run(debug=True)
