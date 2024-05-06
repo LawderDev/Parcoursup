dbc094bb572a16713c7c432d6ecf78a7193602b7
Kenny Ramin <kenny.ramin433@gmail.com>
2024-05-06 13:47:33 +0200

    fix btn size

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-   < <h1 class="text-3xl font-bold w-[100vw] h-[100vh] flex items-center justify-center">{{ state.helloWorld }}</h1>>
+      <ButtonPrimary title="Ajouter un projet"></ButtonPrimary>
   </div>
 </template>
 
