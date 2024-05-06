7a089f80646421fa76c304d8a65004f01961cf96
noan-miage <n.lecarboulec@gmail.com>
2024-05-06 16:22:01 +0200

    page d'admin

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-   < <h1 class="text-3xl font-bold w-[100vw] h-[100vh] flex items-center justify-center">{{ state.helloWorld }}</h1>>
+    <NavBar></NavBar>
+    <div class="flex justify-end mr-10">
+      <ButtonPrimary title = "Ajouter un projet"> </ButtonPrimary>
+    </div>
+    <div class="mt-6">
+      <SessionItem title = "Projet TIC 2024" endDate = "Fin de 12/05/2024"></SessionItem>
+      <SessionItem title = "Projet TIC 2023" endDate = "Fin de 12/05/2024"></SessionItem>
+      <SessionItem title = "Projet TIC 2022" endDate = "Fin de 12/05/2024"></SessionItem>
+      <SessionItem title = "Projet TIC 2021" endDate = "Fin de 12/05/2024"></SessionItem>
+      <SessionItem title = "Projet TIC 2020" endDate = "Fin de 12/05/2024"></SessionItem>
+    </div>
+    <div class="flex justify-center">
+      <ButtonPlus class="md:hidden neumorphism"></ButtonPlus>
+    </div>
   </div>
-</template>
+</template> 
 
 <script setup>
 import axios from "axios";
