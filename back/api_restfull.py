2e33231e0774577c3cbb397ea31701c47bcd3e62
Kenny Ramin <kenny.ramin433@gmail.com>
2024-05-07 14:27:30 +0200

    fix merge

--- back/api_restfull.py
+++ back/api_restfull.py
@@ -83,5 +83,55 @@ def gale_shapley(women_preferences, men_preferences):
     matched_pairs = [{'man': pair[0], 'woman': pair[1]} for pair in proposals.keys()]
     return {'matched_pairs': matched_pairs}
 
+
+@app.route('/api/create_group', methods=['POST'])
+# Example of data and post request to call in the front : 
+# const data = {
+#       "sessionID":1,
+#       "emails":["Benjamin.Bancal@etu.u-bordeaux.fr","Sandra.Ly@etu.u-bordeaux.fr"]
+#     };
+#     const jsonData = JSON.stringify(data);
+
+#     const response = await axios.post("http://127.0.0.1:5000/api/create_group", jsonData, {
+#       headers: {
+#         'Content-Type': 'application/json'
+#       }}
+#     );
+def create_group():
+    print('Enter create group function')
+
+    # Retrieve parameters from the request body
+    sessionID = request.json.get('sessionID')  # assuming the parameters are sent in JSON format
+    studentEmails = request.json.get('emails')
+
+    # Il faut utiliser os.path.join pour que ce soit multiplateforme
+    db = os.path.join(os.getcwd(), 'db', 'parcoursup.sqlite') 
+    if os.path.exists(db):
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+
+        try:
+            # Create the group in the table GROUPE and return the ID
+            sqlRequest = cursor.execute("INSERT INTO GROUPE VALUES (NULL, NULL, NULL, NULL) RETURNING ID")
+            groupID = sqlRequest.fetchone()
+
+            # Update ETUDIANT table with the group ID for this SESSION
+            queryParameters = [(groupID[0], email, sessionID) for email in studentEmails]
+
+            sqlRequest = cursor.executemany("UPDATE ETUDIANT SET FK_Groupe = ? WHERE Email = ? and FK_Session = ?", queryParameters)
+            res = sqlRequest.fetchone()
+
+            # Commit the insertions
+            conn.commit()
+            conn.close()
+
+            # Convert data to JSON format
+            return jsonify({'result': "done"}), 200
+
+        except sqlite3.Error as e:
+            return jsonify({'error': str(e)}), 500   
+    else:
+        return jsonify({'error': "nul"}), 50
+
 if __name__ == '__main__':
     app.run(debug=True)
