6f45f5a08657d1db03394a2d88482edfb24bd6ac
noan-miage <n.lecarboulec@gmail.com>
2024-05-13 15:33:49 +0200

    début page groupe

---
+++ front/components/Steps.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex justify-center">
+        <ul class="steps">
+            <li class="step step-primary"></li>
+            <li class="step step-primary"></li>
+            <li class="step"></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
---
+++ front/pages/createGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <h2 class="text-3xl my-8 font-extrabold text-center">GROUPE</h2>
+      <h3 class="m-5 text-gray-500 text-center">
+        Renseignez les informations ci-dessous afin de créer votre demande de projet
+      </h3>
+    </div>
+    <div class="flex justify-center">
+        <Card>
+            <Steps></Steps>
+            <div class="divider"></div> 
+            <h2 class="text-xl my-4 font-bold ml-2">Votre groupe</h2>
+            <h3 class="ml-2 text-gray-500">De qui est composé votre groupe?</h3>
+            
+        </Card>
+        
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
