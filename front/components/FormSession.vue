bd1014230ee6727d72e13488bd2a273d814f9989
Lucas-Matusiak <lucassmatusiak@gmail.com>
2024-05-16 11:23:07 +0200

    suppression message d'erreur file quand edit

--- front/components/FormSession.vue
+++ front/components/FormSession.vue
@@ -115,7 +115,7 @@
             d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-        <span v-if="!fileCorrect" class=""
+        <span v-if="!fileCorrect && !props.editMode" class=""
           >Veuillez renseigner un fichier valide</span
         >
         <span v-else>Erreur inconnue.</span>
