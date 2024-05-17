e75c3a66c26dd5357f9770c5f94907bb4b5cee50
noan-miage <n.lecarboulec@gmail.com>
2024-05-17 16:06:14 +0200

    page résultats

---
+++ front/pages/result.vue
@@ -0,0 +1,21 @@
+<template>
+    <ProjectsConfirmation 
+        title="RESULTATS"
+        subTitleMobile="Télécharger l'assignation des projets"
+        subTitle="Visualiser et télécharger l'assignation des projets"
+        cardTitle="Les projets ont bien été assignés" 
+        downloadButton="Télécharger le résultat"
+        buttonTitle="Retour" 
+        :nb-steps="2"
+        :nb-steps-active="2"
+        :nb-steps-lock="0"
+        @handleButtonClick="previousStep">
+    </ProjectsConfirmation>
+</template>
+
+<script setup>
+const previousStep = async () => {
+    await navigateTo('/rankingProjects')
+}
+
+</script>
