b90a593a2f099fa111187378d906abc26f06eb2d
KennyR <58228730+LawderDev@users.noreply.github.com>
2024-05-17 11:33:08 +0200

    logic add group in progress

--- front/pages/createGroup/[sessionId].vue
+++ front/pages/createGroup/[sessionId].vue
@@ -7,12 +7,10 @@
     sub-title="Renseignez les informations ci-dessous afin de créer votre demande de projet"
     button-title="Valider le groupe"
     @handle-button-click="validateGroup">
-
       <FormCreateGroup title="Votre groupe" subTitle="De qui est composé votre groupe ?"></FormCreateGroup>
   </StudentsActions>
 </template>
 
 <script setup>
-
 
 </script>
