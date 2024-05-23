8d16c4f4832f1317df0a842fcb66279f73b4c43f
Vo1ucia <157504371+Vo1ucia@users.noreply.github.com>
2024-05-23 14:29:57 +0200

    Date limite qui marche

---
+++ front/pages/createGroup/closed.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <h1>Group Creation Closed</h1>
+      <p>The deadline for creating groups has passed. Please contact your admin for further information.</p>
+    </div>
+  </template>
+  
